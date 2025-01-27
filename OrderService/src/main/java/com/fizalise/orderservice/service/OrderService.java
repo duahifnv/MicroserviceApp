0fd842f42f6b9dba740526614ed0058eaf6fc074
Максим Фоминцев <max135603@gmail.com>
2025-01-27 16:06:52 +0300

    FeignClient для InventoryService

--- OrderService/src/main/java/com/fizalise/orderservice/service/OrderService.java
+++ OrderService/src/main/java/com/fizalise/orderservice/service/OrderService.java
@@ -1,18 +1,26 @@
 package com.fizalise.orderservice.service;
 
+import com.fizalise.orderservice.client.InventoryClient;
 import com.fizalise.orderservice.dto.OrderRequest;
 import com.fizalise.orderservice.entity.Order;
 import com.fizalise.orderservice.repository.OrderRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @Slf4j
-public record OrderService(OrderRepository orderRepository) {
+public record OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
     public Order placeOrder(OrderRequest orderRequest) {
+        if (!inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Item with SKU code = %s is not in stock or stock does not have such quantity"
+                            .formatted(orderRequest.skuCode()));
+        }
         Order order = Order.builder()
                 .orderNumber(UUID.randomUUID().toString())
                 .price(orderRequest.price())
