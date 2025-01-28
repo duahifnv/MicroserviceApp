24a5f2128c265edcee79fe2d3ed61e57623a5454
Максим Фоминцев <max135603@gmail.com>
2025-01-29 02:31:04 +0300

    Добавлены маршруты без аутентификации

--- ApiGateway/src/main/java/com/fizalise/apigateway/config/SecurityConfig.java
+++ ApiGateway/src/main/java/com/fizalise/apigateway/config/SecurityConfig.java
@@ -8,13 +8,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
+            "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.oauth2ResourceServer(
                         oauth2 -> oauth2.jwt(Customizer.withDefaults())
                 )
                 .authorizeHttpRequests(
-                        authorize -> authorize.anyRequest().authenticated()
+                        authorize -> authorize
+                                .requestMatchers(freeResourceUrls).permitAll()
+                                .anyRequest().authenticated()
                 ).build();
     }
 }
