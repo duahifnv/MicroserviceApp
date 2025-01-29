5118ce7832b242c59a16aab5ddd93617f0d981a1
Максим Фоминцев <max135603@gmail.com>
2025-01-30 01:35:43 +0300

    Разрешены все HTTP методы для CORS

--- ApiGateway/src/main/java/com/fizalise/apigateway/config/SecurityConfig.java
+++ ApiGateway/src/main/java/com/fizalise/apigateway/config/SecurityConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -19,6 +24,17 @@ public class SecurityConfig {
                         authorize -> authorize
                                 .requestMatchers(freeResourceUrls).permitAll()
                                 .anyRequest().authenticated()
-                ).build();
+                )
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.applyPermitDefaultValues();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
