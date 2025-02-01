a612d8580f5d87dbc0cebf000fcfc1f67acf424c
Максим Фоминцев <max135603@gmail.com>
2025-02-01 03:47:27 +0300

    Конфигурация обсерватора

---
+++ NotificationService/src/main/java/com/fizalise/notificationservice/config/ObservationConfig.java
@@ -0,0 +1,23 @@
+package com.fizalise.notificationservice.config;
+
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.aop.ObservedAspect;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+@Configuration
+@RequiredArgsConstructor
+public class ObservationConfig {
+    private final ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory;
+    @PostConstruct
+    public void setObservationForKafkaTemplate() {
+        kafkaListenerContainerFactory.getContainerProperties().setObservationEnabled(true);
+    }
+    @Bean
+    ObservedAspect observedAspect(ObservationRegistry registry) {
+        return new ObservedAspect(registry);
+    }
+}
