5ae9048ca4226904486c4a7ef56aeac5046cedee
Максим Фоминцев <max135603@gmail.com>
2025-02-01 03:48:31 +0300

    Добавлена обсервация для RestClient

--- OrderService/src/main/java/com/fizalise/orderservice/config/RestClientConfig.java
+++ OrderService/src/main/java/com/fizalise/orderservice/config/RestClientConfig.java
@@ -1,6 +1,8 @@
 package com.fizalise.orderservice.config;
 
 import com.fizalise.orderservice.client.InventoryClient;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,16 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
+@RequiredArgsConstructor
 public class RestClientConfig {
     @Value("${inventory-service.url}")
     private String inventoryServiceUrl;
+    private final ObservationRegistry observationRegistry;
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .observationRegistry(observationRegistry)
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
